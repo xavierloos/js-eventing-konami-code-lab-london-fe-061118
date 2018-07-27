c4aa871af3a69352c27bbb827cb6be8f21ed98d7
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-27 15:39:37 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+document.getElementsByTagName("body").addEventListener("keydown", myFunction);
 
+function keydownFunction() {
+    alert("sorry")
+}
 }
