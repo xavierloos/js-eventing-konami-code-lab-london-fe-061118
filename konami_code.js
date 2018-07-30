1caf4a60ffd1663d4911f1a86f0bc982caa8ac56
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-30 18:19:18 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ function init() {
   const body = document.querySelector('body')
   
   body.addEventListener('keydown', function(event){
-    if(code ==38){
+    if(keydown == code){
       alert("I'M 38")
     }
     alert('HEY')
