e2d3c8a150c3bb9d5be55649d18895d238176d18
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-30 18:34:06 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,16 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65] 
 let index = 0; 
-function init() { const body = document.querySelector('body') body.addEventListener('keydown', function(event){ const key = parseInt(event.detail || event.which); if (key === code[index]){ index++; if (index === code.length){ alert('Hurray'); index = 0; } } }) } init()
+function init() { 
+  const body = document.querySelector('body') 
+  body.addEventListener('keydown', function(event){ 
+    key = parseInt(event.detail || event.which);
+    if (key === code[index]){ 
+      index++;
+      if (index === code.length){ 
+        alert('Hurray'); 
+        index = 0; 
+        
+      } 
+      
+    } }) } init()
 
